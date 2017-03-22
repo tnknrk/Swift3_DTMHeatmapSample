4296146e06e5cdee75f7b83c5809a4ae14a216f7
N.TANAKA <noirpur@gmail.com>
2017-03-23 00:22:50 +0900

    pod files

---
+++ Pods/Target Support Files/DTMHeatmap/DTMHeatmap-umbrella.h
@@ -0,0 +1,13 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+#import "DTMHeatmapRenderer.h"
+#import "DTMDiffHeatmap.h"
+#import "DTMHeatmap.h"
+#import "DTMColorProvider.h"
+#import "DTMDiffColorProvider.h"
+
+FOUNDATION_EXPORT double DTMHeatmapVersionNumber;
+FOUNDATION_EXPORT const unsigned char DTMHeatmapVersionString[];
+
